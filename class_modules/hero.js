917a58b7125a380f3a3ea41f8ab2b26b6bef45a6
Patrick Murray <patrick.murrayjr@gmail.com>
2023-04-28 18:35:25 -0400

    Updated comments for Hero and Squad classes

--- class_modules/hero.js
+++ class_modules/hero.js
@@ -1,15 +1,28 @@
+/*  Hero Class
+    Models a Superhero. 
+    
+    author: Patrick Murray
+
+*/
 class Hero {
    constructor(heroName, secretIdentity) {
       this.heroName = heroName;
       this.secretIdentity = secretIdentity;
-      this.powers = [];
    }
 
+   /* editHero Method 
+      Used to update the heroName and secretIdentity proprties.
+      If the function call does not include a newSecretIdentity then
+      the secretIdentity property will remain unchanged 
+   */
    editHero(newHeroName, newSecretIdentity = this.secretIdentity) {
       this.heroName = newHeroName;
       this.secretIdentity = newSecretIdentity;
    }
 
+   /* viewHero Method
+      returns the heroName and secretIdentity as a formatted string
+   */
    viewHero() {
       return `${this.heroName}: (${this.secretIdentity})\n`;
    }
--- class_modules/squad.js
+++ class_modules/squad.js
@@ -1,10 +1,21 @@
 import Hero from "./hero.js";
+
+/*  Squad Class
+    Models a collection of Superhero objects. 
+    
+    author: Patrick Murray
+
+*/
 class Squad {
    constructor(squadName) {
       this.squadName = squadName;
       this.roster = [];
    }
 
+   /* addHero Method 
+      expects a Hero object as a param, throws an error if the param is not of type Hero
+      adds the argument onto the roster array
+   */
    addHero(hero) {
       if (hero instanceof Hero) {
          this.roster.push(hero);
@@ -13,27 +24,41 @@ class Squad {
       }
    }
 
+   /* deleteHero Method 
+      requires an index as a param
+      if the element exists at the given index, it is removed from the array
+   */
    deleteHero(index) {
       if (this.roster[index] !== undefined) {
          this.roster.splice(index, 1);
       }
    }
 
+   /* editSquadName Method 
+      expects a newSquadName parameter
+      sets the property squadName to be newSquadName
+   */
    editSquadName(newSquadName) {
       this.squadName = newSquadName;
    }
 
+   /* viewSquad Method 
+      returns the squadName and number of elements in the roster array as a formatted string
+   */
    viewSquad() {
       return `${this.squadName} - Heroes: ${this.roster.length}`;
    }
 
+   /* viewSquadRoster Method 
+      loops through the roster array and builds a string containing all elements in the 
+      roster array using the viewHero method to retrieve a formatted element
+      returns the built string || returns 'No Heroes' if the roster array is empty
+   */
    viewSquadRoster() {
       let rosterList = "";
       if (this.roster.length > 0) {
-         //rosterList += "--------\n";
          this.roster.forEach((member, index) => {
             rosterList += `${index + 1}) ${member.viewHero()}`;
-            // rosterList += `${member.viewHero()}`;
          });
       } else {
          rosterList = "No Heroes";
